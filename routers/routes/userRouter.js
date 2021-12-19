3224578d4d7ea22e0e17196805ccf83700f9d3e9
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2021-12-19 11:44:47 +0300

    add

---
+++ routers/routes/userRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const userRouter = express.Router();
+const {userAuth} = require('../../auth/userAuth')
+
+const {saveUser, getAllUsers, getDataForLogIn} = require('../controllers/userController')
+
+userRouter.post("/", getDataForLogIn)
+userRouter.get("/", userAuth, getAllUsers)
+userRouter.post("/save", saveUser)
+
+module.exports= {userAuth};
