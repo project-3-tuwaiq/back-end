a84166c01517cacaeaa63365912b5340cba51318
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2021-12-26 06:03:22 +0300

    lastUpdate

--- routers/routes/userRouter.js
+++ routers/routes/userRouter.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const userRouter = express.Router();
-// const {userAuth} = require('../../auth/userAuth')
-const {saveUser, getAllUsers, getDataForLogIn, changeUserInfo} = require('../controllers/userController')
+const {userAuth} = require('../../auth/authUser')
+const {saveUser, getAllUsers, getDataForLogIn, changeUserInfo, getUserDataFromToken} = require('../controllers/userController')
 
 
 userRouter.post("/", getDataForLogIn)
 userRouter.get("/", getAllUsers)
 userRouter.post("/save", saveUser)
 userRouter.put("/:id", changeUserInfo)
+userRouter.get("/userAuth",userAuth, getUserDataFromToken)
 
-
-module.exports= {userRouter};
+module.exports= {userRouter, userAuth};
